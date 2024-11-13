d63930b44a8d539b9be4d61f222436a9e0b2125c
Arthur Ott <arthur.ott91@web.de>
2024-11-13 20:46:12 +0100

    initial commit and more

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+  <HeroSection />
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import HeroSection from "./components/HeroSection.vue";
+
+export default {
+  components: { NavBar, HeroSection },
+};
+</script>
+<style scoped></style>
